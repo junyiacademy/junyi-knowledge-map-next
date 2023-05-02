6b1fa56da7f153a2f3c0e81c42b3a4adaab32b0c
kwanlok.ng <kwanlok.ng@junyiacademy.org>
2023-05-02 12:58:07 +0800

    set up github action to deploy the app.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    loader: 'akamai',
+    path: '',
+  },
+  assetPrefix: './',
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
