ae3d6118f259ff1c0ecf4d2efd9a55ece5bd8b04
kwanlok.ng <kwanlok.ng@junyiacademy.org>
2023-05-03 16:25:36 +0800

    New: Fetch the csv data by selected csv path, and generate the road map.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,52 +1,79 @@
 import React, { useEffect, useState } from 'react';
 import type { NextPageWithLayout } from 'next';
 import { Edge, Node } from 'vis-network';
-import { Box } from '@mui/material';
+import { Box, CircularProgress, MenuItem } from '@mui/material';
 import useFetchData from '@/hooks/useFetchData';
+import { dataFilePathList, fileNameList } from '@/utils/road-map/const';
 import Layout from '@/components/layer/Layout';
 import RoadMap from '@/components/road-map';
+import Select from '@/components/ui/select/Select';
 
 interface CustomNode extends Node {
   name: string;
 }
 
-type CsvData = {
+type NodeData = {
   nodes: CustomNode[];
   edges: Edge[];
 };
 
 const Home: NextPageWithLayout = () => {
-  const { data: queryData, isSuccess } = useFetchData();
-  const [csvData, setCsvData] = useState<CsvData>({ edges: [], nodes: [] });
+  const [selectedClass, setSelectedClass] = useState<string>(
+    Object.keys(dataFilePathList)[0]
+  );
+  const {
+    data: csvData,
+    isSuccess,
+    isLoading,
+  } = useFetchData(dataFilePathList[selectedClass]);
+  const [nodeData, setNodeData] = useState<NodeData>({ edges: [], nodes: [] });
 
   useEffect(() => {
-    if (isSuccess && queryData) {
-      const rows = queryData.split('\n');
-      const uniqueSet = new Set();
-      const csvArray: CsvData = { nodes: [], edges: [] };
-      for (let i = 1; i < rows.length; i++) {
-        const rowData = rows[i].replace(/\r/g, '').split(',');
-        csvArray.edges.push({ from: rowData[2], to: rowData[5] });
-        if (!uniqueSet.has(rowData[2])) {
-          uniqueSet.add(rowData[2]);
-          csvArray.nodes.push({
+    if (!isLoading && isSuccess && csvData) {
+      const csvDataList = csvData.split('\n');
+      const uniqueNodeSet = new Set();
+      const nodeList: NodeData = { nodes: [], edges: [] };
+      for (let i = 1; i < csvDataList.length; i++) {
+        const rowData = csvDataList[i].replace(/\r/g, '').split(',');
+
+        // If the current node exists, add it to the node list and show on the road map
+        if (!rowData[2]) {
+          continue;
+        }
+        // Add the current node to the node list if it does not exist
+        if (!uniqueNodeSet.has(rowData[2])) {
+          uniqueNodeSet.add(rowData[2]);
+          nodeList.nodes.push({
             id: rowData[2],
-            label: `${rowData[0]}-${rowData[1]}`,
-            name: `${rowData[2]}-${rowData[0]}-${rowData[1]}`,
+            label: `${rowData[0]} - ${rowData[1]}`,
+            name: `${rowData[2]} - ${rowData[0]}-${rowData[1]}`,
           });
         }
-        if (!uniqueSet.has(rowData[5])) {
-          uniqueSet.add(rowData[5]);
-          csvArray.nodes.push({
+
+        // If the previous node exists, add it to the node list and show on the road map
+        if (!rowData[5]) {
+          continue;
+        }
+        // Add the previous node to the node list if it does not exist
+        if (!uniqueNodeSet.has(rowData[5])) {
+          uniqueNodeSet.add(rowData[5]);
+          nodeList.nodes.push({
             id: rowData[5],
-            label: `${rowData[3]}-${rowData[4]}`,
-            name: `${rowData[5]}-${rowData[3]}-${rowData[4]}`,
+            label: `${rowData[3]} - ${rowData[4]}`,
+            name: `${rowData[5]} - ${rowData[3]}-${rowData[4]}`,
           });
         }
+
+        // Add the edge between the current node and the previous node
+        nodeList.edges.push({ from: rowData[2], to: rowData[5] });
       }
-      setCsvData(csvArray);
+      setNodeData(nodeList);
     }
-  }, [queryData]);
+  }, [csvData]);
+
+  const handleSelectChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSelectedClass(event.target.value);
+  };
 
   return (
     <Box
@@ -55,7 +82,34 @@ const Home: NextPageWithLayout = () => {
         height: '100%',
       }}
     >
-      <RoadMap roadMapData={csvData} />
+      <Select
+        value={selectedClass}
+        helperText="選擇班級"
+        SelectProps={{
+          onChange: handleSelectChange,
+        }}
+        paperMaxHeight={330}
+      >
+        <MenuItem value="none" disabled>
+          請選擇班級
+        </MenuItem>
+        {fileNameList.map((fileName) => (
+          <MenuItem key={fileName.id} value={fileName.id}>
+            {fileName.name}
+          </MenuItem>
+        ))}
+      </Select>
+      {isLoading ? (
+        <Box>
+          <CircularProgress
+            sx={{
+              marginTop: '50px',
+            }}
+          />
+        </Box>
+      ) : (
+        <RoadMap roadMapData={nodeData} />
+      )}
     </Box>
   );
 };
---
+++ utils/road-map/const.ts
@@ -0,0 +1,22 @@
+export const dataFilePathList: {
+  [key: string]: string;
+} = {
+  '1_TO_2': '/dbfb35noxz0fplx/output_1%2C2.csv',
+  '3_TO_4': '/n2mrrelsd4mf947/output_3%2C4.csv',
+  '5_TO_6': '/o4eo7c0modmmzyc/output_5%2C6.csv',
+  TO_7: '/v77hvxm27faqes4/output_to_7.csv',
+  '7_TO_9': '/3hqc6chywzy1dez/output_to_7%2C8%2C9.csv',
+  '10_TO_12': '/ieo4a6aa5b909ac/output_to_10%2C11%2C12.csv',
+};
+
+export const fileNameList: {
+  id: string;
+  name: string;
+}[] = [
+  { id: '1_TO_2', name: '一至二年級' },
+  { id: '3_TO_4', name: '三至四年級' },
+  { id: '5_TO_6', name: '五至六年級' },
+  { id: 'TO_7', name: '至七年級' },
+  { id: '7_TO_9', name: '七至九年級' },
+  { id: '10_TO_12', name: '十至十二年級' },
+];
