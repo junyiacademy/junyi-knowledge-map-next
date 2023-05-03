97452ff09f509a06bd452e365b1836bf93e11de0
kwanlok.ng <kwanlok.ng@junyiacademy.org>
2023-05-03 16:24:46 +0800

    Refactor: csv data can fetch by dropbox path name.

--- hooks/useFetchData.ts
+++ hooks/useFetchData.ts
@@ -1,23 +1,17 @@
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 
-const fetchData = async () => {
-  const response = await axios.get(
-    'https://dl.dropbox.com/s/gtycnw048vvdqer/output567.csv'
-  );
+const getCsvDataKey = 'getUserProgressCacheEndpoint';
+
+const fetchData = async (filePath: string) => {
+  const dropBoxEndpoint = 'https://dl.dropbox.com/s';
+  const response = await axios.get(`${dropBoxEndpoint}${filePath}`);
   return response.data;
 };
 
-export default function useFetchData() {
-  const response = useQuery({
-    queryKey: ['getUserProgressCacheEndpoint'],
-    queryFn: () => fetchData(),
+export default function useFetchData(filePath: string) {
+  return useQuery({
+    queryKey: [getCsvDataKey, filePath],
+    queryFn: () => fetchData(filePath),
   });
-
-  return {
-    data: response?.data,
-    isLoading: response?.isLoading,
-    isSuccess: response?.isSuccess,
-    refetch: response?.refetch,
-  };
 }
